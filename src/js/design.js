922e6526d96f36792313e574f96d5633db31820d
Laura Murray <mlauramurray@gmail.com>
2025-09-20 13:57:43 +0200

    local storage

--- src/js/design.js
+++ src/js/design.js
@@ -1,22 +1,23 @@
 'use strict';
 
 // Variables para los checkboxes
-let styleOne = document.querySelector('.style-one');
-let styleTwo = document.querySelector('.style-two');
-let styleThree = document.querySelector('.style-three');
+const styleOne = document.querySelector('.style-one');
+const styleTwo = document.querySelector('.style-two');
+const styleThree = document.querySelector('.style-three');
 
 // Variables para los iconos
-let githubIcon = document.querySelector('.github-icon');
-let emailIcon = document.querySelector('.email-icon');
-let phoneIcon = document.querySelector('.phone-icon');
-let slackIcon = document.querySelector('.slack-icon');
+const githubIcon = document.querySelector('.github-icon');
+const emailIcon = document.querySelector('.email-icon');
+const phoneIcon = document.querySelector('.phone-icon');
+const slackIcon = document.querySelector('.slack-icon');
 
 // Variable para la preview card
-let previewCardBackground = document.querySelector('.preview-card-image');
-let logoAdalabWhite = document.querySelector('.logo-adalab');
+const previewCardBackground = document.querySelector('.preview-card-image');
+const logoAdalabWhite = document.querySelector('.logo-adalab');
 
 // Función para cambiar tarjeta según el estilo
 function updateIcons(style) {
+  console.log( style);
   if (style === 'style-one') {
     styleOne.checked = true;
     styleTwo.checked = false;
@@ -51,8 +52,16 @@ function updateIcons(style) {
     previewCardBackground.classList.remove('preview-card-image_style2');
     logoAdalabWhite.src = 'images/logo-adalab-negativo.png';
   }
+    localStorage.setItem("style",style);
 };
 
 styleOne.addEventListener('click', () => updateIcons('style-one'));
 styleTwo.addEventListener('click', () => updateIcons('style-two'));
 styleThree.addEventListener('click', () => updateIcons('style-three'));
+
+const styleSaved = localStorage.getItem("style");
+if(styleSaved) {
+ updateIcons(styleSaved);
+
+};
+
--- src/js/form.js
+++ src/js/form.js
@@ -1,6 +1,5 @@
-'use strict';
-console.log("ready");
-
+  'use strict';
+  console.log("ready");
 
   // Seleccion inputs
 const inputImage = document.querySelector(".input-image");
@@ -17,16 +16,67 @@ const resetInfoBtn = document.querySelector(".reset-info");
 
 //seleccion de clases del html de la tarjeta
 
-let cardName = document.querySelector(".card-name");
-let rol = document.querySelector(".card-rol");
-let gitHubUser = document.querySelector(".card-github");
-let phone = document.querySelector(".card-phone");
-let eMail = document.querySelector(".card-email");
-let slackUser = document.querySelector(".card-slack");
-let profileImage = document.querySelector(".blank-profile-pic");
+const cardName = document.querySelector(".card-name");
+const rol = document.querySelector(".card-rol");
+const gitHubUser = document.querySelector(".card-github");
+const phone = document.querySelector(".card-phone");
+const eMail = document.querySelector(".card-email");
+const slackUser = document.querySelector(".card-slack");
+const profileImage = document.querySelector(".blank-profile-pic");
 const previewCard = document.querySelector(".preview-card");
 const originalPreviewCardHTML = previewCard.innerHTML;
 //const formDesign = document.querySelector(".form-design-card");
+
+const dataPreviewDefault = {
+  username: 'María',
+  rol: 'front-end', 
+  gitHubUser: 'https://github.com/maria',
+  email: 'maria@gmail.com',
+  phone: '555 555 555',
+  slack: '@_maria',
+  profilePic: 'images/blank-profile-pic.png',
+  selectedStyle: 'style-one',
+  };
+
+  const saveToLocalStorage = () => {
+  const data = {
+    image: profileImage.src,
+    name: inputName.value,
+    rol: inputRol.value,
+    github: inputGitHub.value,
+    mail: inputMail.value,
+    tel: inputTel.value,
+    slack: inputSlack.value,
+  };
+  localStorage.setItem("cardData", JSON.stringify(data));
+  console.log("Guardado en localStorage:", data);
+};
+
+
+const loadFromLocalStorage = () => {
+  const saved = localStorage.getItem("cardData");
+  console.log("Recuperado de localStorage:", saved);
+
+  if (!saved) return;
+  const data = JSON.parse(saved);
+
+    // Rellenar inputs
+    inputName.value = data.name || ""; 
+    inputRol.value = data.rol || "";
+    inputGitHub.value = data.github || "";
+    inputMail.value = data.mail || "";
+    inputTel.value = data.tel || "";
+    inputSlack.value = data.slack || "";
+
+    // Rellenar tarjeta
+    cardName.textContent = data.name || dataPreviewDefault.username;
+    rol.textContent = data.rol || dataPreviewDefault.rol;
+    gitHubUser.textContent = data.github || dataPreviewDefault.gitHubUser;
+    eMail.textContent = data.mail || dataPreviewDefault.email;
+    phone.textContent = data.tel || dataPreviewDefault.phone;
+    slackUser.textContent = data.slack || dataPreviewDefault.slack;
+    profileImage.src = data.image || dataPreviewDefault.profilePic;
+}
 
 
 //eventos
@@ -38,16 +88,30 @@ const handleImageChange = (e) => {  //agregue la funcion manejadora que me corri
     const reader = new FileReader();
     reader.onload = (e) => {
       profileImage.src = e.target.result;
-      profileImage.style.maxWidth = "120px";
-     /*  saveToLocalStorage(); */
+   
+      saveToLocalStorage(); 
     };
     reader.readAsDataURL(file);
   }
 };
 inputImage.addEventListener("change", handleImageChange);
 
-// Nombre
-const handleNameInput = () => {
+[inputName, inputRol, inputGitHub, inputMail, inputTel, inputSlack].forEach(input => {
+  input.addEventListener("input", () => {
+    // Reflejar en tarjeta
+    cardName.textContent = inputName.value || dataPreviewDefault.username;
+    rol.textContent = inputRol.value || dataPreviewDefault.rol;
+    gitHubUser.textContent = inputGitHub.value || dataPreviewDefault.gitHubUser;
+    eMail.textContent = inputMail.value || dataPreviewDefault.email;
+    phone.textContent = inputTel.value || dataPreviewDefault.phone;
+    slackUser.textContent = inputSlack.value || dataPreviewDefault.slack;
+
+    saveToLocalStorage();
+  });
+});
+
+//* / Nombre
+/* const handleNameInput = () => {
   cardName.textContent = inputName.value;
   console.log(cardName);
   console.log(inputName);
@@ -82,18 +146,9 @@ inputMail.addEventListener("input", handleMailInput);
 const handleTelInput = () => {
   phone.textContent = inputTel.value;
 };
-inputTel.addEventListener("input", handleTelInput);
+inputTel.addEventListener("input", handleTelInput); */
+ 
 
-const dataPreviewDefault = {
-  username: 'María',
-  rol: 'front-end', 
-  gitHubUser: 'https://github.com/maria',
-  email: 'maria@gmail.com',
-  phone: '555 555 555',
-  slack: '@_maria',
-  profilePic: 'images/blank-profile-pic.png',
-  selectedStyle: 'style-one',
-  };
 
 /*resetBtn.addEventListener("click", () => {
   form.reset();            // limpia los inputs
@@ -119,7 +174,7 @@ const dataPreviewDefault = {
 });*/
 
 //Resetear con el botón
-resetBtn.addEventListener("click", () => {
+/* resetBtn.addEventListener("click", () => {
   form.reset();             // limpia los inputs
   inputImage.value = "";    // asegura que el file input quede vacío
   cardName.textContent = dataPreviewDefault.username;
@@ -130,9 +185,26 @@ resetBtn.addEventListener("click", () => {
   slackUser.textContent = dataPreviewDefault.slack;
   profileImage.src = dataPreviewDefault.profilePic;
   updateIcons(dataPreviewDefault.selectedStyle);
+}); */
+
+resetBtn.addEventListener("click", () => {
+  form.reset();
+  profileImage.src = dataPreviewDefault.profilePic;
+  cardName.textContent = dataPreviewDefault.username;
+  rol.textContent = dataPreviewDefault.rol;
+  gitHubUser.textContent = dataPreviewDefault.gitHubUser;
+  eMail.textContent = dataPreviewDefault.email;
+  phone.textContent = dataPreviewDefault.phone;
+  slackUser.textContent = dataPreviewDefault.slack;
+  updateIcons(dataPreviewDefault.selectedStyle);
+  localStorage.removeItem("cardData");
 });
 
+   
 
+
+// Cargar automáticamente los datos al iniciar la página
+ document.addEventListener("DOMContentLoaded", loadFromLocalStorage); 
 
 
 
