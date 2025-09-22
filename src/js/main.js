e82322fd828966d9618942add71c20c740dc15ff
micaelaalvarezj <micaela.alvarezj@gmail.com>
2025-09-22 17:37:20 +0200

    botón descargar tarjeta funcionando

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,4 @@ import './form.js';
 import './design.js';
 import './collapsed-sections.js';
 import './reset-download.js';
+import './share.js';
---
+++ src/js/share.js
@@ -0,0 +1,16 @@
+import domtoimage from 'dom-to-image';
+
+const downloadButton = document.querySelector(".download-card-button");
+downloadButton.addEventListener("click", (event) => {
+    console.log("click en el botón descargar");
+    event.preventDefault ();
+
+domtoimage
+.toJpeg(document.querySelector('.preview-card'), { quality: 0.95 })
+    .then( (dataUrl) => {
+        const link = document.createElement('a');
+        link.download = 'tu-firma.jpeg';
+        link.href = dataUrl;
+        link.click();
+    });
+});
